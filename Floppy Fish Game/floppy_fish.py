a4981d453a3a9d66bdaef79fa8bc16fd0cc2640e
YoItsPeter <67535287+YoItsPeter@users.noreply.github.com>
2023-02-25 22:48:10 -0500

    Update floppy_fish.py
    collision detection complete which then ends program

--- Floppy Fish Game/floppy_fish.py
+++ Floppy Fish Game/floppy_fish.py
@@ -20,7 +20,8 @@ NET_VEL = -5
 NET_GAP = 260
 
 # Game Events
-NEW_NET = pygame.USEREVENT + 1
+COLLISION_NET = pygame.USEREVENT + 1
+COLLISION_ROD = pygame.USEREVENT + 2
 
 # Sound Bytes
 FISH_FLOP_SOUND = pygame.mixer.Sound(os.path.join('Assets\\floppy_fish_assets', 'floppy_fish_peter.mp3'))
@@ -38,7 +39,7 @@ BOT_NET_SPRITE = pygame.transform.flip(pygame.transform.rotate(NET_SPRITE, 180),
 BOT_ROD_SPRITE = pygame.transform.flip(pygame.transform.rotate(ROD_SPRITE, 180), True, False)
 
 
-def handle_nets(top_nets_active, top_rods_active, bot_nets_active, bot_rods_active):
+def handle_nets(top_nets_active, top_rods_active, bot_nets_active, bot_rods_active, fish):
     num_net = 0
     new_pos = None
     if num_net % 4 == 0:
@@ -50,6 +51,9 @@ def handle_nets(top_nets_active, top_rods_active, bot_nets_active, bot_rods_acti
             net.x = 1100
             net.y = new_pos
             num_net += 1
+        
+        if fish.colliderect(net):
+            pygame.event.post(pygame.event.Event(COLLISION_NET))
 
     for rod in top_rods_active:
         rod.x += NET_VEL
@@ -58,6 +62,9 @@ def handle_nets(top_nets_active, top_rods_active, bot_nets_active, bot_rods_acti
             rod.y = -400 + new_pos
             num_net += 1
 
+        if fish.colliderect(rod):
+            pygame.event.post(pygame.event.Event(COLLISION_ROD))
+
     for net in bot_nets_active:
         net.x += NET_VEL
         if net.x + NET_WIDTH <= 0:
@@ -65,12 +72,18 @@ def handle_nets(top_nets_active, top_rods_active, bot_nets_active, bot_rods_acti
             net.y = new_pos + NET_GAP
             num_net += 1
 
+        if fish.colliderect(net):
+            pygame.event.post(pygame.event.Event(COLLISION_NET))
+
     for rod in bot_rods_active:
         rod.x += NET_VEL
         if rod.x + NET_WIDTH <= 0:
             rod.x = 1100
             rod.y = new_pos + NET_GAP
             num_net += 1
+
+        if fish.colliderect(rod):
+            pygame.event.post(pygame.event.Event(COLLISION_ROD))
 
 
 def draw_window(fish, top_nets_active, top_rods_active, bot_nets_active, bot_rods_active, background_x, background_x2):
@@ -98,6 +111,7 @@ def main():
     playing = False
     background_x = 0
     background_x2 = 2400
+    collision = 0
 
     # Hit Boxes
     fish = pygame.Rect(200, 300, 40, 40)
@@ -105,15 +119,15 @@ def main():
     nets_active = [pygame.Rect(700, pos_1, NET_WIDTH, NET_HEIGHT),
                    pygame.Rect(1100, pos_2, NET_WIDTH, NET_HEIGHT),
                    pygame.Rect(1500, pos_3, NET_WIDTH, NET_HEIGHT),
-                   pygame.Rect(700, -400 + pos_1, 10, 139),
-                   pygame.Rect(1100, -400 + pos_2, 10, 139),
-                   pygame.Rect(1500, -400 + pos_3, 10, 139),
+                   pygame.Rect(700, -400 + pos_1, 10, 400),
+                   pygame.Rect(1100, -400 + pos_2, 10, 400),
+                   pygame.Rect(1500, -400 + pos_3, 10, 400),
                    pygame.Rect(700, pos_1 + NET_GAP, NET_WIDTH, NET_HEIGHT),
                    pygame.Rect(1100, pos_2 + NET_GAP, NET_WIDTH, NET_HEIGHT),
                    pygame.Rect(1500, pos_3 + NET_GAP, NET_WIDTH, NET_HEIGHT),
-                   pygame.Rect(700, pos_1 + NET_GAP, 10, 200),
-                   pygame.Rect(1100, pos_2 + NET_GAP, 10, 200),
-                   pygame.Rect(1500, pos_3 + NET_GAP, 10, 200)]
+                   pygame.Rect(700, pos_1 + NET_GAP, 10, 400),
+                   pygame.Rect(1100, pos_2 + NET_GAP, 10, 400),
+                   pygame.Rect(1500, pos_3 + NET_GAP, 10, 400)]
     top_nets_active, top_rods_active = (nets_active[0:3]), (nets_active[3:6])
     bot_nets_active, bot_rods_active = (nets_active[6:9]), (nets_active[9:12])
 
@@ -130,6 +144,12 @@ def main():
                     fish_momentum -= 27
                     FISH_FLOP_SOUND.play()
                     playing = True
+
+            if event.type == COLLISION_ROD:
+                run = False
+
+            if event.type == COLLISION_NET:
+                 run = False
 
         if playing:
             # Fish Momentum
@@ -157,7 +177,9 @@ def main():
             if background_x == -2400:
                 background_x = background_x2 + 2400
             
-            handle_nets(top_nets_active, top_rods_active, bot_nets_active, bot_rods_active)
+            handle_nets(top_nets_active, top_rods_active, bot_nets_active, bot_rods_active, fish)
+            
+
 
         draw_window(fish, top_nets_active, top_rods_active, bot_nets_active, bot_rods_active, background_x, background_x2)
 
